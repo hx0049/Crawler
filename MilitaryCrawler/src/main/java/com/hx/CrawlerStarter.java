0ee02a4acd10498e4b10d1ccba538e9911fb8f96
hx0049 <huangxiang49@126.com>
2019-03-23 14:56:00 +0800

    Update CrawlerStarter.java

--- MilitaryCrawler/src/main/java/com/hx/CrawlerStarter.java
+++ MilitaryCrawler/src/main/java/com/hx/CrawlerStarter.java
@@ -18,7 +18,20 @@ public class CrawlerStarter {
         String url = "http://s.huanqiu.com/s?q="+URLEncoder.encode(keyWord,"utf-8");
         System.out.println(IOUtils.toString(new URL(url),"utf-8"));*/
 
-        /*String[] keyWords = {"民兵3", "弹道弹道"};
+        System.out.println("军事爬虫已启动，请选择爬取网址");
+        System.out.println("1 环球网（关键字搜索爬取，内置关键字为【民兵3】和【弹道弹道】，爬取数据量较少）");
+        System.out.println("2 新浪网（不支持关键字爬取，默认爬取【中国军情】，【国际军情】，【东海局势】，【南海局势】）");
+        Scanner sc = new Scanner(System.in);
+        if(sc.nextInt() == 1){
+            crawHQ();
+        }else{
+            crawSina();
+        }
+
+
+    }
+    public static void crawHQ() throws Exception{
+        String[] keyWords = {"民兵3", "弹道弹道"};
         List<String> urls = new ArrayList<>();
         for (String keyWord : keyWords) {
             for (int i = 0; i < 10; i++) {
@@ -28,8 +41,10 @@ public class CrawlerStarter {
         }
 
         AbstractNewsService newsService = new HQNewsService();
-        newsService.startCrawler(urls);*/
+        newsService.startCrawler(urls);
+    }
 
+    public static void crawSina(){
         Map<String,String> keyWordMap = new HashMap<>();
         keyWordMap.put("中国军情","57918");
         keyWordMap.put("国际军情","57919");
